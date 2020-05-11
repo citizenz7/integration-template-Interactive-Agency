8e4eb7d28c60f27929bd2009c4aa5e9ce0582e18
citizenz7 <citizenz7@protonmail.com>
2020-05-11 16:50:20 +0200

    integration 2 php files

---
+++ main.php
@@ -0,0 +1,29 @@
+<?php include 'header.php'; ?>
+
+
+  <div class="row">
+    <div id="bestdesign" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+      <div class="p-5 text-light">
+        <div class="pl-5">
+          <h1>BestDesign</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <a href="#" class="myButton">Ask for price</a>
+        </div>
+      </div>
+
+    </div>
+    <div id="bestcode" class="col-sm-12 col-md-6 m-0 p-0 w-100 h-75">
+      <div class="p-5 text-light">
+        <div class="pr-5">
+          <h1>BestCode</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <a href="#" class="myButton">Ask for price</a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
+
+<?php include 'footer.php';
